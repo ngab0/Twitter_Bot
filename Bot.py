b8fd2f1e7047352a8f9bf65ab72694be188d8627
NGABO <56110802+ngab0@users.noreply.github.com>
2022-02-12 13:11:17 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,3 +1,4 @@
+#import necessary modules
 import tweepy
 import time
 
@@ -5,26 +6,30 @@ from keys import *
 
 print("This is my first python project", flush=True)
 
+#use the object to communicate with twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+#contains consumer and access key, secret info
 file_name = "last_seen_id.txt"
 
+#check last time timeline was checked
 def retrieve_last_seen_id(file_name):
     f_read = open(file_name, 'r')
     last_seen_id = int(f_read.read().strip())
     f_read.close()
     return last_seen_id
 
+#save last time timeline was checked
 def store_last_seen_id(last_seen_id, file_name):
     f_write = open(file_name, 'w')
-    f_write.write(str(last_seen_id))
+    f_write.write(str(last_seen_id))        #convert las_seen_id to a string
     f_write.close()
     return
 
-
-def reply_to_tweets():
+#function that replies to retweets
+def retweet():
     print("retrieving and replying to tweets...", flush=True)
     
     last_seen_id = retrieve_last_seen_id(file_name)
@@ -40,5 +45,5 @@ def reply_to_tweets():
             api.retweet(tweet.id)
      
 while True:
-    reply_to_tweets()
+    retweet()
     time.sleep(30)
